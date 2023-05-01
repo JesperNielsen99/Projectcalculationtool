9e0f85f405db765a022060e5d7269f9b7d776a48
KEATTLoevkilde <thom4a14@stud.kea.dk>
2023-05-01 13:57:52 +0200

    added SubprojectController with createSubproject

---
+++ src/main/java/com/example/projectcalculationtool/controllers/SubprojectController.java
@@ -0,0 +1,33 @@
+package com.example.projectcalculationtool.controllers;
+
+import com.example.projectcalculationtool.models.Subproject;
+import com.example.projectcalculationtool.services.SubprojectService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SubprojectController {
+    private SubprojectService subprojectService;
+
+    public SubprojectController(SubprojectService subprojectService) {
+        this.subprojectService = subprojectService;
+    }
+
+    @GetMapping("/createSubproject")
+    public String createSubproject(Model model){
+        Subproject subproject = new Subproject();
+        model.addAttribute("subproject", subproject);
+        return "redirect:/createSubprojectForm";
+    }
+
+    @PostMapping("/createSubproject")
+    public String createSubproject(@ModelAttribute Subproject subproject){
+        subprojectService.createSubproject(subproject);
+        return "redirect:/createSubprojectForm"; // TODO: 01-05-2023 change redirect to project
+    }
+
+
+}
