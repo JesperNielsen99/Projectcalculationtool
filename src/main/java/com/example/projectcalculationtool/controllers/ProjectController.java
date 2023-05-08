dfd818785eb38d247db7c236e21a01e0b8d6c580
Hjordten <andr314e@stud.kea.dk>
2023-05-08 12:34:05 +0200

    Minor reactor to make better naming convention

--- src/main/java/com/example/projectcalculationtool/controllers/ProjectController.java
+++ src/main/java/com/example/projectcalculationtool/controllers/ProjectController.java
@@ -2,17 +2,11 @@ package com.example.projectcalculationtool.controllers;
 
 
 import com.example.projectcalculationtool.models.Project;
-import com.example.projectcalculationtool.models.Task;
 import com.example.projectcalculationtool.services.ProjectService;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -24,18 +18,18 @@ public class ProjectController {
     }
 
 
-    @GetMapping("/projectsPage")
+    @GetMapping("/projects")
     public String showProjects(Model model){
         int ID = 1;
         List<Project> projects = projectService.getProjects(ID);
         model.addAttribute("projectList", projects);
-        return "projectPage";
+        return "projects";
     }
 
 
     /* ------------------------------------ Create project ----------------------------------------- */
 
-    @GetMapping ("/projectsPage/createProject")
+    @GetMapping ("/project/create")
     public String createProject(Model model){ //TODO Establish how we get managerID
 
         Project project = new Project();
@@ -45,23 +39,25 @@ public class ProjectController {
         return "createProjectForm"; //TODO Make sure to get managerID trasnfered to form
     }
 
-    @PostMapping("/projectsPage/createProject")
+    @PostMapping("/project/create")
     public String addProject(@ModelAttribute Project project){
         projectService.createProject(project);
-        return "redirect:/projectsPage"; //TODO change redirect to homepage
+        return "redirect:/projects"; //TODO change redirect to homepage
     }
 
-    @GetMapping("projectsPage/updateProject")
+    /* ------------------------------------ Update project ----------------------------------------- */
+
+    @GetMapping("project/update")
     public String updateProjectForm(@RequestParam int projectID, Model model){
         Project project = projectService.getProject(projectID);
         model.addAttribute("project", project);
         return "updateProjectForm";
     }
 
-    @PostMapping("/projectsPage/updateProject")
+    @PostMapping("/project/update")
     public String updateProjectSubmit(@ModelAttribute Project project){
         projectService.updateProject(project);
-        return "redirect:/projectsPage"; //TODO needs a 'mainPage' as landing page + an ID
+        return "redirect:/projects"; //TODO needs a 'mainPage' as landing page + an ID
     }
 
 
