37838b5653abfbf59b2c9c3a62d236cef313abe7
JesperZamora <jesp78m6@stud.kea.dk>
2023-05-08 10:22:50 +0200

    rearranged task tests

--- src/test/java/com/example/projectcalculationtool/repositories/TaskRepositoryTest.java
+++ src/test/java/com/example/projectcalculationtool/repositories/TaskRepositoryTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -29,11 +32,43 @@ class TaskRepositoryTest {
         db_connector.setUrl("jdbc:mysql://localhost:3306/tasktest_db");
         db_connector.setUser("root");
         db_connector.setPass("Jw-180490");
-        TaskTestDB testDB = new TaskTestDB();
-        testDB.taskTestDB();
+        taskTestDB();
 
         task1 = new Task(1,1,"T-Task1", "T-Description1", 1,1, LocalDate.now(),false);
         task2 = new Task(2,1,"T-Task2", "T-Description2", 1,1, LocalDate.now(),false);
+    }
+
+    public void taskTestDB(){
+        try{
+            Connection conn = DB_Connector.getConnection();
+
+            Statement statement = conn.createStatement();
+
+            conn.setAutoCommit(false);
+
+            statement.addBatch("SET foreign_key_checks = 0;");
+
+            statement.addBatch("DROP TABLE IF EXISTS task");
+
+            statement.addBatch("CREATE TABLE task (\n" +
+                    "\ttask_id INTEGER NOT NULL AUTO_INCREMENT,\n" +
+                    "    subproject_id INT NOT NULL,\n" +
+                    "    task_name VARCHAR(255) NOT NULL,\n" +
+                    "    task_description VARCHAR(255) NOT NULL,\n" +
+                    "    task_priority INT NOT NULL,\n" +
+                    "    task_duration INT NOT NULL,\n" +
+                    "    task_deadline DATE NOT NULL,\n" +
+                    "    task_completed TINYINT NOT NULL,\n" +
+                    "    PRIMARY KEY (task_id),\n" +
+                    "    FOREIGN KEY (subproject_id) REFERENCES subproject (subproject_id) ON DELETE CASCADE ON UPDATE CASCADE\n" +
+                    ");");
+
+            statement.executeBatch();
+            conn.commit();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
