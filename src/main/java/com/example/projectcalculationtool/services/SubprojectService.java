462743d8cf20770a35c2bafaf365a0f47fa658a7
KEATTLoevkilde <thom4a14@stud.kea.dk>
2023-05-10 10:52:50 +0200

    repo and service done

--- src/main/java/com/example/projectcalculationtool/services/SubprojectService.java
+++ src/main/java/com/example/projectcalculationtool/services/SubprojectService.java
@@ -4,6 +4,8 @@ import com.example.projectcalculationtool.models.Subproject;
 import com.example.projectcalculationtool.repositories.interfaces.ISubprojectRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SubprojectService {
     private ISubprojectRepository subprojectRepository;
@@ -14,6 +16,22 @@ public class SubprojectService {
 
     public void createSubproject(Subproject subproject){
         subprojectRepository.createSubproject(subproject);
+    }
+
+    public List<Subproject> getSubprojects(int projectID){
+        return subprojectRepository.getSubprojects(projectID);
+    }
+
+    public Subproject getSubproject(int subprojectID){
+        return subprojectRepository.getSubproject(subprojectID);
+    }
+
+    public void updateSubproject(Subproject subproject){
+        subprojectRepository.updateSubproject(subproject);
+    }
+
+    public void deleteSubproject(int subprojectID){
+        subprojectRepository.deleteSubproject(subprojectID);
     }
 
 }
