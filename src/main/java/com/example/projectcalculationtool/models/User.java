2efac088d5b28034ce27b15ec0fd7519fbdf2024
Jesper Nielsen <jesp74m8@stud.kea.dk>
2023-05-31 13:24:46 +0200

    added setter for userID so that updateUser works

--- src/main/java/com/example/projectcalculationtool/models/User.java
+++ src/main/java/com/example/projectcalculationtool/models/User.java
@@ -24,6 +24,10 @@ public class User {
         return userID;
     }
 
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
     public String getFirstName() {
         return firstName;
     }
